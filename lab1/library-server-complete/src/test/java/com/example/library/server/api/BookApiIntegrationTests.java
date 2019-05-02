b80800b0c81d2828a7767f042079cd36b5556464
Andreas Falk <andreas.falk@novatec-gmbh.de>
2019-05-02 08:39:23 +0200

    fix failing tests (mock jwt stuff for now)

--- lab1/library-server-complete/src/test/java/com/example/library/server/api/BookApiIntegrationTests.java
+++ lab1/library-server-complete/src/test/java/com/example/library/server/api/BookApiIntegrationTests.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.DirtiesContext;
@@ -39,6 +41,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BookApiIntegrationTests {
 
   @Autowired private WebApplicationContext context;
+
+  @SuppressWarnings("unused")
+  @MockBean
+  private JwtDecoder jwtDecoder;
 
   private MockMvc mockMvc;
 
